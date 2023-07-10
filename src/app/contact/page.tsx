e7b0f6a823e4c40e3eacb294fcda0d7f235b5a52
Grayson-choi <784wldnd@gmail.com>
2023-07-10 23:47:39 +0900

    contact page 테스트

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,12 +5,11 @@ export const metadata = {
 
 export default function ContactPage() {
   return (
-  <div>
-    <div className="bg-gray-100 p-10 rounded-lg">
-    <h1 className="mt-12 text-center text-3xl font-bold">Contact</h1>
-      <p className="text-gray-700 text-lg">이메일: jojaeso@kakao.com</p>
+    <div>
+      <div className="rounded-lg bg-gray-100 p-10">
+        <h1 className="mt-12 text-center text-3xl font-bold">연락처ㅎ</h1>
+        <p className="text-lg text-gray-700">이메일: jojaeso@kakao.com</p>
+      </div>
     </div>
-  </div>
-  
   );
 }
