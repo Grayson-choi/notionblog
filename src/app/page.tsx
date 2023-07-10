363660d5cfbe17aae3ea8d3790129ec4c6e33b26
Grayson-choi <784wldnd@gmail.com>
2023-07-11 00:16:02 +0900

    메인페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,40 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-// export default function HomePage() {
-//   // return <h1 className="mt-12 text-center text-3xl font-bold">Home</h1>;
-//   return (
-//     <Link href="/blog">
-//       <span className="mr-2">&larr;</span>
-//     </Link>
-//   );
-// }
 import CategoryFilter from '@/components/filter/category-filter';
 import SearchBar from '@/components/filter/search-bar';
 import PostsGrid from '@/components/posts/posts-grid';
 import { getAllPostsFromNotion } from '@/services/posts';
 import { toUniqueArray } from '@/utils/to-unique-array';
 
+export default function HomePage() {
+  // return <h1 className="mt-12 text-center text-3xl font-bold">Home</h1>;
+  return (
+    <Link href="/blog">
+      <span className="mr-2">&larr;</span>
+    </Link>
+  );
+}
+
 export const metadata = {
   title: 'Welcome | UcanCode',
   description: '개발을 통해 함께 성장하는 교육자입니다.',
 };
 
-export default async function BlogPage() {
-  const allPosts = await getAllPostsFromNotion();
+// export default async function BlogPage() {
+//   const allPosts = await getAllPostsFromNotion();
 
-  const allCategories = toUniqueArray(
-    allPosts
-      .filter((post) => post.published)
-      .map((post) => post.categories)
-      .flat()
-  ).sort();
+//   const allCategories = toUniqueArray(
+//     allPosts
+//       .filter((post) => post.published)
+//       .map((post) => post.categories)
+//       .flat()
+//   ).sort();
 
-  return (
-    <>
-      <section className="mb-16 mt-0 space-y-8 md:mt-20">
-        <SearchBar />
-        <CategoryFilter allCategories={allCategories} />
-      </section>
-      <PostsGrid allPosts={allPosts} />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h1>Main 페이지</h1>
+
+//     </>
+//   );
+// }
