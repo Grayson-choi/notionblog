585e9723d71256c5e84399349be15a4890651d31
Grayson-choi <784wldnd@gmail.com>
2023-07-10 23:42:52 +0900

    contact page update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,5 +4,13 @@ export const metadata = {
 };
 
 export default function ContactPage() {
-  return <h1 className="mt-12 text-center text-3xl font-bold">Contact</h1>;
+  return (
+  <div>
+    <div className="bg-gray-100 p-10 rounded-lg">
+    <h1 className="mt-12 text-center text-3xl font-bold">Contact</h1>
+      <p className="text-gray-700 text-lg">이메일: jojaeso@kakao.com</p>
+    </div>
+  </div>
+  
+  );
 }
